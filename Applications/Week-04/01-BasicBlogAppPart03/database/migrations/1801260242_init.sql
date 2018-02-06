1f0aee8297f82175f590b8674fcd9ba9b6ba3ad9
Corey Shuman <qddrum@sbcglobal.net>
2018-02-05 17:24:38 -0800

    working on week 4 app

---
+++ Applications/Week-04/01-BasicBlogAppPart03/database/migrations/1801260242_init.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE posts (
+    id SERIAL NOT NULL,
+    title character varying NOT NULL,
+    post character varying NOT NULL,
+    author character varying NOT NULL,
+    created_at timestamp NOT NULL default now(),
+    updated_at timestamp NOT NULL default now(),
+    deleted_at timestamp NOT NULL default now(),
+    is_deleted boolean DEFAULT false NOT NULL
+);
+
+ALTER TABLE ONLY posts ADD CONSTRAINT post_pkey PRIMARY KEY (id);
+ALTER TABLE posts OWNER TO postgres;
+
+CREATE TABLE users (
+    id SERIAL NOT NULL,
+    username character varying NOT NULL,
+    email character varying NOT NULL,
+    password character varying NOT NULL,
+    created_at timestamp NOT NULL default now(),
+    updated_at timestamp NOT NULL default now(),
+    deleted_at timestamp NOT NULL default now()
+);
+
+ALTER TABLE ONLY users ADD CONSTRAINT user_pkey PRIMARY KEY (id);
+ALTER TABLE users OWNER TO postgres;
