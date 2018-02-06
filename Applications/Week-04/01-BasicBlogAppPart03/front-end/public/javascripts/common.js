75f8ff3e864e5bd3c2104eea1fe515d48288154e
Corey Shuman <qddrum@sbcglobal.net>
2018-02-06 01:38:14 -0800

    added menu, styling, and loading indicator to blog part 3

--- Applications/Week-04/01-BasicBlogAppPart03/front-end/public/javascripts/common.js
+++ Applications/Week-04/01-BasicBlogAppPart03/front-end/public/javascripts/common.js
@@ -1,50 +1,100 @@
 // common api functionality
+
 let MyBlogApp = {};
-MyBlogApp.apiUrl = 'http://localhost:3000/api';
-MyBlogApp.request = function(method, url, payload, handler) {
-    xhr = new XMLHttpRequest();
-    if (!handler && typeof payload === 'function') {
-        handler = payload;
-        payload = null;
-    }
-
-    xhr.open(method, MyBlogApp.apiUrl + url);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
-        handler(xhr.status, JSON.parse(xhr.responseText));
-    };
-    xhr.send(JSON.stringify(payload));
-}
-
-MyBlogApp.getFormInput = function(formEvent, name) {
-    if (formEvent && formEvent.srcElement) {
-        let elements = formEvent.srcElement;
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[i].name === name) {
-                return elements[i].value;
-            }
+(function() {
+    'use strict';
+    MyBlogApp.apiUrl = 'http://localhost:3000/api';
+    MyBlogApp.request = function(method, url, payload, handler) {
+        const xhr = new XMLHttpRequest();
+        if (!handler && typeof payload === 'function') {
+            handler = payload;
+            payload = null;
         }
-        return null;
-    } else {
-        return null;
+
+        xhr.open(method, MyBlogApp.apiUrl + url);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function() {
+            handler(xhr.status, JSON.parse(xhr.responseText));
+        };
+        xhr.send(JSON.stringify(payload));
     }
-}
 
-MyBlogApp.toast = function(level, message) {
-    let toast = document.createElement('div');
-    let toastClose = document.createElement('button');
-    let toastMessage = document.createElement('span');
-    toast.setAttribute('role', 'alert');
-    toast.className = `alert alert-${level} alert-dismissible fade show`;
+    MyBlogApp.getFormInput = function(formEvent, name) {
+        if (formEvent && formEvent.srcElement) {
+            let elements = formEvent.srcElement;
+            for (let i = 0; i < elements.length; i++) {
+                if (elements[i].name === name) {
+                    return elements[i].value;
+                }
+            }
+            return null;
+        } else {
+            return null;
+        }
+    }
 
-    toastClose.textContent = "X";
-    toastClose.className = "close";
-    toastClose.setAttribute('data-dismiss', 'alert');
-    toastClose.setAttribute('aria-label', 'Close');
-    toast.appendChild(toastClose);
+    MyBlogApp.toast = function(level, message) {
+        let toast = document.createElement('div');
+        let toastClose = document.createElement('button');
+        let toastMessage = document.createElement('span');
+        toast.setAttribute('role', 'alert');
+        toast.className = `alert alert-${level} alert-dismissible fade show`;
 
-    toastMessage.textContent = message;
-    toast.appendChild(toastMessage);
+        toastClose.textContent = "X";
+        toastClose.className = "close";
+        toastClose.setAttribute('data-dismiss', 'alert');
+        toastClose.setAttribute('aria-label', 'Close');
+        toast.appendChild(toastClose);
 
-    document.querySelector("div[id='toasts']").appendChild(toast);
-}
+        toastMessage.textContent = message;
+        toast.appendChild(toastMessage);
+
+        document.querySelector("div[id='toasts']").appendChild(toast);
+    }
+
+    /*************** spinner  *******************/
+    MyBlogApp.spinnerCount = 0;
+    MyBlogApp.spinnerOpts = {
+        lines: 5, // The number of lines to draw
+        length: 80, // The length of each line
+        width: 2, // The line thickness
+        radius: 22, // The radius of the inner circle
+        scale: 1.3, // Scales overall size of the spinner
+        corners: 1, // Corner roundness (0..1)
+        color: '#2ef9a1', // CSS color or array of colors
+        fadeColor: 'blue', // CSS color or array of colors
+        opacity: 0.35, // Opacity of the lines
+        rotate: 0, // The rotation offset
+        direction: -1, // 1: clockwise, -1: counterclockwise
+        speed: 1.6, // Rounds per second
+        trail: 100, // Afterglow percentage
+        fps: 20, // Frames per second when using setTimeout() as a fallback in IE 9
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        className: 'spinner', // The CSS class to assign to the spinner
+        top: '50%', // Top position relative to parent
+        left: '50%', // Left position relative to parent
+        shadow: '10px', // Box-shadow for the lines
+        position: 'absolute' // Element positioning
+    };
+
+    MyBlogApp.spinnerTarget = document.getElementById('main-content');
+    MyBlogApp.loadingTarget = document.getElementById('loading-screen');
+    console.log(MyBlogApp.spinnerTarget)
+    MyBlogApp.spinner = new Spinner(MyBlogApp.spinnerOpts);
+
+    MyBlogApp.spin = function() {
+        MyBlogApp.spinnerCount++;
+        if (MyBlogApp.spinnerCount === 1) {
+            MyBlogApp.loadingTarget.style.display = 'block';
+            MyBlogApp.spinner.spin(MyBlogApp.spinnerTarget);
+        }
+    }
+
+    MyBlogApp.spinStop = function() {
+        MyBlogApp.spinnerCount--;
+        if (MyBlogApp.spinnerCount === 0) {
+            MyBlogApp.loadingTarget.style.display = 'none';
+            MyBlogApp.spinner.stop();
+        }
+    }
+}());
--- Applications/Week-04/01-BasicBlogAppPart03/front-end/public/javascripts/login.js
+++ Applications/Week-04/01-BasicBlogAppPart03/front-end/public/javascripts/login.js
@@ -4,9 +4,10 @@ MyBlogApp.loginHandler = function(e) {
     console.log(e);
     let email = MyBlogApp.getFormInput(e, 'email');
     let password = MyBlogApp.getFormInput(e, 'password');
-
+    MyBlogApp.spin();
     MyBlogApp.request('POST', '/user/login', { email: email, password: password }, (status, data) => {
         console.log(status, data);
+        MyBlogApp.spinStop();
         if (status === 200) {
             document.location.href = '/users/welcome?name=' + data.data.username;
         } else if (status === 404) {
@@ -17,5 +18,5 @@ MyBlogApp.loginHandler = function(e) {
 
 // setup the event handler
 window.onload = function() {
-    document.querySelector("form").addEventListener('submit', MyBlogApp.loginHandler);
+    document.querySelector("form#login-form").addEventListener('submit', MyBlogApp.loginHandler);
 }
