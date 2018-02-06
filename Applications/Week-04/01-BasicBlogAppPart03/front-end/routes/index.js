099b8a71adeb21adbba2f5656f6fdf962cf325ae
Corey Shuman <qddrum@sbcglobal.net>
2018-02-05 17:57:57 -0800

    more blog part 3 dev

--- Applications/Week-04/01-BasicBlogAppPart03/front-end/routes/index.js
+++ Applications/Week-04/01-BasicBlogAppPart03/front-end/routes/index.js
@@ -1,37 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var db = require("../db/db");
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    // todo - should try-catch here
-    let data = await db.get();
-    if (data) {
-        console.log(data);
-        res.render('index', Object.assign({ title: 'XSS Sandbox' }, data));
-    } else {
-        console.log("no data");
-        res.render('index', { title: 'XSS Sandbox', content: 'Hello World', attribute: 'bold', css: 'gray', javascript: 'console.log("hello")' });
-    }
-
+    res.render('index', {});
 });
 
 router.post('/', async function(req, res, next) {
     // todo - should try-catch here
     console.log("save", req.body);
-    try {
-        let data = await db.upsert(req.body);
-        if (data) {
-            console.log(data);
-            res.render('index', Object.assign({ title: 'XSS Sandbox' }, data));
-        } else {
-            console.log("no data");
-            res.render('index', { title: 'XSS Sandbox', content: 'Hello World', attribute: 'bold', css: 'gray', javascript: 'console.log("hello")' });
-        }
-    } catch (e) {
-        console.log(e);
-    }
-
+    res.render('index', {});
 });
 
 module.exports = router;
--- Applications/Week-04/01-BasicBlogAppPart03/front-end/routes/users.js
+++ Applications/Week-04/01-BasicBlogAppPart03/front-end/routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login', {});
+});
+
+router.post('/login', function(req, res, next) {
+    res.render('login', {});
 });
 
 module.exports = router;
